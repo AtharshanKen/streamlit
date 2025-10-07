1f692107df5979e2a92b41fbac096e97686c2944
AtharshanKen <atharshan.kennedy@dcmail.ca>
2025-10-06 22:11:19 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"atharshan.kennedy@dcmail.ca\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[general]\nemail = \"your-email@example.com\"\n" > ~/.streamlit/credentials.toml
+echo "[server]\nheadless = true\nenableCORS = false\nport = $PORT\n" > ~/.streamlit/config.toml
